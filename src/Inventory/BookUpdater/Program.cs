2bf284f5f675617d7be4d4fb65c040486f8e4ced
Brad Knowles <code@bradknowles.com>
2021-09-20 10:53:42 -0400

    style(config): 🎨 Setup editorconfig
    Setup editorconfig all other files to support reporting issues during IDE and CI builds.

--- src/Inventory/BookUpdater/Program.cs
+++ src/Inventory/BookUpdater/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+using System;
 
+[assembly: CLSCompliant(true)]
 namespace OpenLMS.Inventory.BookUpdater
 {
     internal class Program
     {
-        private static void Main(string[] args)
+#pragma warning disable IDE0022 // Use expression body for methods
+#pragma warning disable IDE0060,CA1801 // Remove unused parameter
+        private static void Main(String[] args)
+#pragma warning restore IDE0060,CA1801 // Remove unused parameter
         {
             Console.WriteLine("Hello World!");
         }
+#pragma warning restore IDE0022 // Use expression body for methods
     }
 }
