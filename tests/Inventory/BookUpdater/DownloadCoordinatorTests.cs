1c7e373f0cc516ef9f3928a958c7f80484e7aa2f
Brad Knowles <code@bradknowles.com>
2021-11-22 02:08:48 -0500

    test(BookUpdater): ✅ Add DownloadComplete happy path test

--- tests/Inventory/BookUpdater/DownloadCoordinatorTests.cs
+++ tests/Inventory/BookUpdater/DownloadCoordinatorTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Immutable;
+using System.Text;
 
 using Akka.Actor;
 using Akka.TestKit;
@@ -34,6 +36,26 @@ namespace OpenLMS.Inventory.BookUpdater.Tests
             result.DownloadPath.ShouldBe(message.DownloadPath);
             result.CorrelationId.ShouldBe(message.CorrelationId);
             result.CausationId.ShouldBe(message.CausationId);
+        }
+
+        [Fact]
+        public void DownloadCoordinator_DownloadCompleteMsg_SendSaveFileMsg()
+        {
+            TestProbe fileSystemCoordinator = CreateTestProbe();
+            TestProbe downloadUrlActor = CreateTestProbe();
+            var downloadContents = Encoding.UTF8.GetBytes("Downloaded data.").ToImmutableArray();
+
+            var message = new DownloadCoordinator.Messages.DownloadComplete(downloadContents, new UPath("path/to/save/file.ext"),
+                Guid.NewGuid(), Guid.NewGuid());
+
+            IActorRef sut = DownloadCoordinator.Create(Sys, fileSystemCoordinator, downloadUrlActor);
+            sut.Tell(message);
+
+            FileSystemCoordinator.Messages.SaveFile result = fileSystemCoordinator.ExpectMsg<FileSystemCoordinator.Messages.SaveFile>();
+            result.Contents.ShouldBe(downloadContents);
+            result.Filename.ShouldBe(message.DownloadPath);
+            result.CorrelationId.ShouldBe(message.CorrelationId);
+            result.CausationId.ShouldBe(message.MessageId);
         }
 
         [Fact]
